f2e3c6f177fe25c2241c7f8d10e5481d1f1304bd
Holger Neuner <neuner@47gradnord.de>
2019-08-07 13:01:34 +0200

    complet interface

--- src/Http/AdapterInterface.php
+++ src/Http/AdapterInterface.php
@@ -6,5 +6,30 @@ namespace Vrpayment\ContaoIsotopeBundle\Http;
 
 interface AdapterInterface
 {
+    /**
+     * Returns the response data.
+     *
+     * @param string $method
+     * @param string $path
+     * @param array  $data
+     *
+     * @return mixed
+     */
+    public function action(string $method, string $path, array $data = []);
 
+    /**
+     * Returns the access token.
+     *
+     * @return string|null
+     */
+    public function getAccessToken();
+
+    /**
+     * Returns the adapter config.
+     *
+     * @param string|null $key
+     *
+     * @return mixed
+     */
+    public function getConfig($key = null);
 }
